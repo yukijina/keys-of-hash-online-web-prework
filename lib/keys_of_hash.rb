f135ef3d0f204631f2a3a0af65207a954864c82b
Yuki N <lunarbase@outlook.com>
2018-11-12 23:51:59 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,6 +4,8 @@ class Hash
   def keys_of(arguments, *arg)
     # code goes here
     array = []
+    
+    if arg == nil
     self.each do |key, value|  
       if value == arguments
         array << key
