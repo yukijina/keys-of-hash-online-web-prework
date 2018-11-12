22492e7bfd162bbf16387fdd403b89b9ae0ddb75
Yuki N <lunarbase@outlook.com>
2018-11-12 22:53:54 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(arguments, *arg)
     # code goes here
+    array = []
+    self.each do |key, value|  
+      if value == arguments
+        array << key
+      end
+    end  
+    
+  　arg.each do |animal, country|
+  　 if country == arguments && country == arg
+  　    array << animal
+  　 end
+    end
+    array
   end
 end
